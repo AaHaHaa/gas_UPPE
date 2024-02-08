11b37e3c927c6f7636eec3db69f6cad2348ee475
AaHaHaa <46036069+AaHaHaa@users.noreply.github.com>
2024-02-08 09:35:07 -0500

    Add files via upload

---
+++ APL paper data/Find ratio of Raman to electronic/find_ratio.m
@@ -0,0 +1,27 @@
+clearvars; close all;
+
+load('H2.mat');
+
+c = 299792.458;
+pump_wavelength = linspace(800,1300,1000)';
+pump_omega = c./pump_wavelength*2*pi; % THz
+
+%% contributions
+X3 = interp1(f*2*pi,sim.X3,pump_omega);
+permittivity0 = 8.85e-12;
+electronic_contribution = 3/4*permittivity0*X3;
+
+Raman_contribution_real = sum(gas.H2.R.preR*4./(gas.H2.R.omega*1e12))/4 + sum(gas.H2.V.preR./(gas.H2.V.omega*1e12))/4;
+Raman_contribution_imag = sum(-gas.H2.R.preR*4/2*(gas.H2.R.T2*1e-12))*0 + sum(-gas.H2.V.preR(2)/2*(gas.H2.V.T2*1e-12));
+Raman_contribution = Raman_contribution_real + 1i*Raman_contribution_imag;
+
+total_contribution = electronic_contribution + Raman_contribution;
+
+ratio = abs(imag(total_contribution)./real(total_contribution));
+
+figure;
+plot(pump_wavelength,ratio,'linewidth',2,'Color','b');
+xlabel('Pump wavelength (nm)');
+ylabel('Im[\kappa+R]/Re[\kappa+R]');
+set(gca,'fontsize',25);
+xlim([pump_wavelength(1),pump_wavelength(end)]);
---
+++ APL paper data/Find ratio of Raman to electronic/generate_gas_data.m
@@ -0,0 +1,52 @@
+% This code compares the result with David's from Philip Russell's group. 
+
+close all; clearvars;
+
+addpath('../../../MMTools/gas_UPPE_vector/user_helpers','../../../MMTools/gas_UPPE_vector/broadband UPPE algorithm');
+
+%% Setup parameters
+c = 299792458*1e-12; % m/ps
+wavelength_range = [0.6,5]*1e-6; % m
+Nt = 2^14;
+[f0,f_range,time_window,dt] = find_tw_f0(c./wavelength_range,Nt);
+
+sim.f0 = f0;
+%sim.progress_bar = false;
+sim.progress_bar_name = 'David''s duplication';
+sim.pulse_centering = false;
+sim.num_photon_noise_per_band = 1;
+
+num_save = 1;
+fiber.L0 = 0.01; % propagation length
+sim.save_period = fiber.L0/num_save;
+
+f = sim.f0+(-Nt/2:Nt/2-1)'/(Nt*dt); % THz
+t = (-Nt/2:Nt/2-1)'*dt; % ps
+lambda = c./f*1e9; % nm
+
+%% Gas info
+[fiber,sim] = load_default_UPPE_propagate(fiber,sim);
+
+gas.core_radius = 15e-6; % m
+gas.temperature = 288; % K
+gas.pressure = 20*1.013e5; % Pa
+gas.wavelength_order = 6;
+gas.mode_profile_wavelength = 1030e-9; % m
+gas.gas_material = 'H2';
+gas.fiber_type = 'no_coating';
+gas.xy_sampling = 101;
+
+[fiber,sim,gas] = gas_info(fiber,sim,gas,lambda*1e-9);
+fiber.betas = real(fiber.betas); % remove the loss
+
+%% Initial condition and Propagate
+tfwhm1 = 1; % ps
+total_energy = 30e3; % nJ
+pump_wavelength = 1030e-9; % m
+freq_shift = c/pump_wavelength - sim.f0;
+input_field = build_MMgaussian(tfwhm1,time_window,total_energy,1,Nt,{'ifft',freq_shift});
+
+prop_output = UPPE_propagate(fiber,input_field,sim,gas);
+
+%%
+save('H2.mat');
