46d9b264fba06df0961ff3854196272bb06398cf
AaHaHaa <46036069+AaHaHaa@users.noreply.github.com>
2024-02-08 09:13:46 -0500

    Add files via upload

---
+++ APL paper data/Fig. 4_Raman-enhanced SPM/Raman_enhanced_SPM_1030nm_in_paper_short.m
@@ -0,0 +1,63 @@
+close all; clearvars;
+
+addpath('../../../MMTools/gas_UPPE_vector/user_helpers','../../../MMTools/gas_UPPE_vector/broadband UPPE algorithm');
+
+%% Setup parameters
+c = 299792458*1e-12; % m/ps
+wavelength_range = [0.7,3]*1e-6; % m
+Nt = 2^9;
+[f0,f_range,time_window,dt] = find_tw_f0(c./wavelength_range,Nt);
+
+sim.f0 = f0;
+%sim.progress_bar = false;
+sim.progress_bar_name = 'SPM';
+sim.gpuDevice.Index = 1;
+sim.pulse_centering = false;
+sim.adaptive_deltaZ.max_deltaZ = 1e-5;
+
+num_save = 50;
+fiber.L0 = 0.5; % propagation length
+sim.save_period = fiber.L0/num_save;
+
+f = sim.f0+(-Nt/2:Nt/2-1)'/time_window; % THz
+t = (-Nt/2:Nt/2-1)'*dt; % ps
+lambda = c./f*1e9; % nm
+
+%% Gas info
+[fiber,sim] = load_default_UPPE_propagate(fiber,sim);
+
+gas.core_radius = 150e-6; % m
+gas.temperature = 273.15 + 25; % K
+gas.pressure = 20*1e5; % Pa
+gas.wavelength_order = 6;
+gas.mode_profile_wavelength = 1030e-9; % m
+gas.gas_material = 'H2';
+gas.fiber_type = 'MWLW_coating';
+gas.xy_sampling = 101;
+
+[fiber,sim,gas] = gas_info(fiber,sim,gas,lambda*1e-9);
+
+%% Initial condition and Propagate
+log10_tfwhm_lim = log10(0.2);
+tfwhm = 10.^linspace(log10_tfwhm_lim,log10_tfwhm_lim+log10(10),30);
+
+total_energy = 500e3; % nJ
+pump_wavelength = 1030e-9; % m
+freq_shift = c/pump_wavelength - sim.f0;
+input_pulse = build_MMgaussian(tfwhm(1),time_window,total_energy,1,Nt,{'ifft',freq_shift});
+
+func = calc_chirp();
+
+num_repeat = 10;
+prop_output = cell(1,num_repeat);
+    
+[~,chirped_pulse] = func.General( tfwhm(7),ifftshift(f*2*pi),ifft(input_pulse.fields),1 );
+initial_condition.dt = input_pulse.dt;
+initial_condition.fields = chirped_pulse;
+
+for j = 1:num_repeat
+    prop_output{1,j} = UPPE_propagate(fiber,initial_condition,sim,gas);
+end
+
+%%
+save('Raman_enhanced_SPM_1030nm_paper_short.mat');
---
+++ APL paper data/Fig. 4_Raman-enhanced SPM/plotter_1030nm_in_paper_short.m
@@ -0,0 +1,50 @@
+clearvars; close all;
+
+addpath('../../../MMTools/gas_UPPE/user_helpers/');
+
+load('Raman_enhanced_SPM_1030nm_paper_short.mat');
+
+Stokes_intensity = zeros(Nt,num_repeat);
+Stokes_QE = zeros(num_save+1,num_repeat);
+for ii = 1
+    for j = 1:num_repeat
+        initial_photon = trapz(f,abs(fftshift(ifft(prop_output{ii,j}.fields(:,:,1)),1)).^2./f);
+        Stokes_pulse = gaussian_spectral_filter(prop_output{ii,j}, sim.f0, c/(c/pump_wavelength-gas.H2.V.omega(2)/2/pi)*1e9, 400, 5);
+
+        Stokes_intensity(:,j) = abs(Stokes_pulse.fields(:,:,end)).^2;
+        
+        Stokes_QE(:,j) = squeeze(trapz(f,abs(fftshift(ifft(Stokes_pulse.fields),1)).^2./f)/initial_photon);
+    end
+end
+Stokes_intensity_mean = mean(Stokes_intensity,2); Stokes_intensity_std = std(Stokes_intensity,[],2);
+norm_factor = max(Stokes_intensity_mean+Stokes_intensity_mean);
+
+Stokes_QE_mean = mean(Stokes_QE,2); Stokes_QE_std = std(Stokes_QE,[],2);
+
+figure;
+confplot(t,Stokes_intensity_mean/norm_factor,Stokes_intensity_std/norm_factor,'linewidth',2,'Color','r');
+hold on;
+plot(t,abs(prop_output{1}.fields(:,:,1)).^2/max(abs(prop_output{1}.fields(:,:,1)).^2),'linewidth',2,'Color','k');
+ylabel('Peak power (norm.)');
+ylim([0,1]);
+set(gca,'fontsize',25);
+xlim([-4,4]);
+xlabel('Time (ps)');
+print(gcf,'pulse_1030nm_short.pdf','-dpdf');
+
+figure;
+confplot(prop_output{1}.z,Stokes_QE_mean*100,Stokes_QE_std*100,'linewidth',2,'Color','r');
+ylabel('QE (%)');
+set(gca,'fontsize',25);
+xlim([0,fiber.L0]);
+xlabel('Propagation distance (m)');
+print(gcf,'QEevolution_1030nm_short.pdf','-dpdf');
+this_ylim = get(gca,'YLim');
+
+figure;
+confplot(prop_output{1}.z,Stokes_QE_mean*100,Stokes_QE_std*100,'linewidth',2,'Color','r');
+set(gca,'fontsize',40);
+xlim([0.4,fiber.L0]);
+ylim(this_ylim);
+set(gca,'XTick',[0.4,0.5])
+print(gcf,'QEevolution_1030nm_short_closeview.pdf','-dpdf');
