8c970647cea8d8770ce435af40772d216f69cf6a
AaHaHaa <46036069+AaHaHaa@users.noreply.github.com>
2024-10-03 08:40:31 -0400

    Update calc_D_op.m

--- broadband UPPE algorithm/calc_D_op.m
+++ broadband UPPE algorithm/calc_D_op.m
@@ -2,8 +2,12 @@ function [D_op,sim] = calc_D_op(fiber,sim,dt,omegas,fields)
 %CALC_D_OP It computes the dispersion operator used in UPPE
 
 if ~isfield(sim,'betas')
-    % Obtain the betas of the input pulse
-    sim.betas = calc_sim_betas(fiber,dt,omegas,fields);
+    if ~any(fields) % fields is all-zero
+        sim.betas = [0;0];
+    else
+        % Obtain the betas of the input pulse
+        sim.betas = calc_sim_betas(fiber,dt,omegas,fields);
+    end
 end
 
 D_op = 1i*(ifftshift(fiber.betas,1)-(sim.betas(1)+sim.betas(2)*omegas));
